0892bc6a35b8a9c799ce88df46e49afcb20dcbde
Rheamer <step.rheamer@gmail.com>
2022-03-27 23:24:49 +0300

    init fix

---
+++ dplm_desktop/main.cpp
@@ -0,0 +1,26 @@
+#include "usbexplorer.h"
+#include "python_utilities.h"
+#include <QApplication>
+#include <QDebug>
+#include <QLocale>
+#include <QTranslator>
+#include <iostream>
+#include "./ui_dplm.h"
+#include <exception>
+
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    Py_Initialize();
+    auto* ui = new Ui::Dplm;
+    Dplm* w = new Dplm(ui);
+    w->setAttribute(Qt::WA_DeleteOnClose);
+    w->show();
+    int execSignal = 0;
+    execSignal = a.exec();
+    if (Py_FinalizeEx() < 0 )
+        exit(120);
+    std::cout << "Finished UsbConnect..." << std::endl;
+    return execSignal;
+}
